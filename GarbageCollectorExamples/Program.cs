ea2d5d9dbcb9d097efc6c981629703c57f149985
Jose guzman <jose.guzman@doosan.com>
2020-07-16 21:07:12 -0500

    initial commit: solution file + gitignore + garbage collector

---
+++ GarbageCollectorExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GarbageCollectorExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RecursiveSum(4000));
+            Console.ReadKey();
+        }
+
+        static int RecursiveSum(int n)
+        {
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            Console.WriteLine("Total bytes Thread: {0}", GC.GetAllocatedBytesForCurrentThread());
+            return n <= 0 ? 0 : (n + RecursiveSum(n - 1));
+        }
+    }
+}
---
+++ dotnet-examples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace dotnet_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(4000));
+            Console.ReadKey();
+        }
+
+        static int Sum(int n)
+        {
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            Console.WriteLine("Total bytes Thread: {0}", GC.GetAllocatedBytesForCurrentThread());
+            return n <= 0 ? 0 : (n + Sum(n - 1));
+        }
+    }
+}
