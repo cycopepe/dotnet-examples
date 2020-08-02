4a3284ab38d99cb4c6ac34484a1152f2457a0af1
Jose guzman <jose.guzman@doosan.com>
2020-08-01 23:00:02 -0500

    add api base

---
+++ TodoApp/TodoApp/API/Controllers/TasksController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dataLayer;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly tododbContext _context;
+
+        public TasksController(tododbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<dataLayer.Task>>> GetTask()
+        {
+            return await _context.Task.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<dataLayer.Task>> GetTask(int id)
+        {
+            var task = await _context.Task.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Tasks/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(int id, dataLayer.Task task)
+        {
+            if (id != task.TaskId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<dataLayer.Task>> PostTask(dataLayer.Task task)
+        {
+            _context.Task.Add(task);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TaskExists(task.TaskId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTask", new { id = task.TaskId }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<dataLayer.Task>> DeleteTask(int id)
+        {
+            var task = await _context.Task.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Task.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return task;
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Task.Any(e => e.TaskId == id);
+        }
+    }
+}
